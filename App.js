97ff1691522428e00da790cbb7c3b140da71aab9
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-09-04 10:43:30 -0500

    added custom colors

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to my App</Text>
+      <Text style={{marginTop:spacing[4]}}>My First React Native project</Text>
       <StatusBar style="auto" />
     </View>
   );
