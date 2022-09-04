01c5c317ab309eaecf759be14f2963e3782cbb4c
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-09-04 11:16:26 -0500

    added text.js file

---
+++ src/components/text/text.js
@@ -0,0 +1,10 @@
+import { View, Text as RNText } from 'react-native'
+import React from 'react'
+
+export default function Text() {
+  return (
+    <View>
+      <RNText>TEXT</RNText>
+    </View>
+  )
+}
