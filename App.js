d3be8e57c2268f09fdca4525970f69a6285f887b
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-09-04 11:10:04 -0500

    added

--- App.js
+++ App.js
@@ -2,21 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/theme/colors';
 import { spacing } from './src/theme/spacing';
-import { useFonts } from 'expo-font';
+
+
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
-    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
-    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
-  });
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to my App</Text>
-      <Text style={{marginTop:spacing[4]}}>My First React Native project</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ 
+
+
+  
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize:spacing[5]}}>Welcome to my App</Text>
+        <Text style={{marginTop:spacing[4]}}>My First React Native project</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  
+ 
 }
 
 const styles = StyleSheet.create({
