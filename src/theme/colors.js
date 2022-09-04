765461b4c4b90dadefacf0a89b1e837f4444ff74
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-09-04 10:27:29 -0500

    added colors plate

---
+++ src/theme/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+black:'#070724',
+white:'#fff',
+darkGrey:'#38384F',
+lightGreen:'#419EBB',
+grey:'#838391',
+orange:'#EDA249',
+purple:'#6f2ed6',
+darkOrange:'#D14C32',
+red:'#D83A34',
+lightRed:'CD5120',
+green:'#1ec2a4',
+blue:'#2d68f0'
+};
