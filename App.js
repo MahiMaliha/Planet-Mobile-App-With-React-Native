8a91df1fffd8f20b446315aaa9076484e0a84ca2
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-09-04 10:52:56 -0500

    installed expo fonts and tested in app.js

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/theme/colors';
 import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
   return (
     <View style={styles.container}>
       <Text>Welcome to my App</Text>
